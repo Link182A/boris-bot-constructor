4cbbaec0ee963f8e890a1b56fa1b0936ff7bd947
Andrew <cc6.magister@gmail.com>
2020-03-20 13:47:32 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# boris-bot-contractor
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn dev
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Run your unit tests
+```
+yarn test:unit
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="hello">
+		<h1>{{ msg }}</h1>
+		<h3>Installed CLI Plugins</h3>
+		<ul>
+			<li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank"
+			       rel="noopener">babel</a></li>
+			<li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
+			       target="_blank" rel="noopener">typescript</a></li>
+			<li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank"
+			       rel="noopener">router</a></li>
+			<li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank"
+			       rel="noopener">vuex</a></li>
+			<li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank"
+			       rel="noopener">unit-jest</a></li>
+		</ul>
+	</div>
+</template>
+
+<script lang="ts">
+
+	export default {
+		name: 'HelloWorld',
+		props: {
+			msg: String,
+		},
+	};
+
+</script>
+
+<style scoped lang="scss">
+	h3 {
+		margin: 40px 0 0;
+	}
+
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+
+	li {
+		display: inline-block;
+		margin: 0 10px;
+	}
+
+	a {
+		color: #42b983;
+	}
+</style>
