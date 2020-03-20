1aa11456d2e1f6ff8a6e2126db6e298dfcee02f3
Andrew <cc6.magister@gmail.com>
2020-03-20 18:18:38 +0200

    add font + icon + main styles

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,23 @@
 		<div id="nav">
 			<router-link to="/">Home</router-link>
 			|
-			<router-link to="/about">About</router-link>
+			<router-link to="/1/constructor">About</router-link>
 		</div>
 		<router-view/>
 	</div>
 </template>
 
-<style lang="scss">
-	#app {
-		font-family: Avenir, Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-	}
-
-	#nav {
-		padding: 30px;
-
-		a {
-			font-weight: bold;
-			color: #2c3e50;
-
-			&.router-link-exact-active {
-				color: #42b983;
-			}
+<script>
+	export default {
+		name: 'App',
+		mounted() {
+			console.log(process.env);
+			console.log(this.$q.lang.isoName);
+			// console.log(this.$t('test'));
 		}
 	}
+</script>
+
+<style lang="scss">
+	@import "styles/main";
 </style>
